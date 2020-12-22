67db019d9ca9bf9afa8c426ea556ff489184d9a5
ecemnurNaz <ecemnurnaz@gmail.com>
2020-12-23 00:44:42 +0300

    first commit

---
+++ OOP_Intro/Ayakkabi.cs
@@ -0,0 +1,30 @@
+﻿
+namespace OOP_Intro
+{
+    class Ayakkabi
+    {
+        //bir ayakkabının ........'sı olur.
+
+        //Bir class tanımladığınız class içerisinde bulunan bütün değişkenlerin birer erişim belirleyicileri bulunmaktadır. 
+        //Access Modifiers (Erişim Belirleyiciler)
+        /*
+            Public => Açık, (Her yerden ulaşılabilir.)
+            Private=> Özel. Bir yapının başına private erişim belirleyicisi tanımlanırsa o yapı sadece bağlı bulunduğu class içerisinden ulaşılır.
+            Internal=> internal olarak tanımlanan bir property ya da class sadece bağlı bulunduğu proje ieçrisinden ulaşılır.
+        -------------------
+            Protected=> protected oalrak işaretlenen bir yapı sadece miras alınan class'dan ulaşılabilir.
+            Protected Internal=> miras alınan ve aynı proje içerisinde bulunan classlarda kullanılır.
+
+            Class içerisinde tanımlanan değişkenler varsayılan olarak "private" şeklinde tanımlanır.
+         */
+
+       public int numara;
+       public string renk;
+       public decimal fiyat;
+       public bool bagcikliMi;
+       public string malzeme;
+       public string marka;
+       public string tip;
+
+    }
+}
---
+++ OOP_Intro/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP_Intro
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Ayakkabi ayakkabi = new Ayakkabi();
+            ayakkabi.bagcikliMi = true;
+            ayakkabi.fiyat = 500;
+            ayakkabi.malzeme = "suni deri";
+            ayakkabi.marka = "Nixe";
+            ayakkabi.renk = "siyah";
+            ayakkabi.tip = "spor";
+
+            //MessageBox.Show(ayakkabi.marka);
+
+            string format = string.Format("Marka:{0} Malzeme:{1} Fiyat:{2}", ayakkabi.marka, ayakkabi.malzeme, ayakkabi.fiyat);
+            listBox1.Items.Add(format);
+        }
+    }
+}
